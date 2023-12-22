16ac3c5953590777d0e483522fd8b30114a75f9e
MCT32 <32090502+MCT32@users.noreply.github.com>
2023-12-22 14:28:20 +1100

    updated colors to use nord theme

--- config.h
+++ config.h
@@ -13,15 +13,17 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char nord0[]           = "#2e3440";
+static const char nord1[]           = "#3b4252";
+static const char nord2[]           = "#434c5e";
+static const char nord3[]           = "#4c566a";
+static const char nord5[]           = "#e5e9f0";
+static const char nord6[]           = "#eceff4";
+static const char nord10[]          = "#5e81ac";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { nord5,     nord0,     nord1     },
+	[SchemeSel]  = { nord6,     nord10,    nord10    },
 };
 
 /* tagging */
@@ -63,7 +65,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", nord0, "-nf", nord5, "-sb", nord10, "-sf", nord6, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
